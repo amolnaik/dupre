b326b2f1b405e42103e63ccdfc57fa26004cd0ec
Naik <amnaik@4demamnaik1.ad.here.com>
2019-11-22 22:10:40 +0100

    checking in modified imagededup

---
+++ imagededup/mkdocs/run_docs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cp ../README.md docs/index.md
+cp ../CONTRIBUTING.md docs/CONTRIBUTING.md
+cp ../LICENSE docs/LICENSE.md
+cp -R ../readme_figures docs/
+python autogen.py
+mkdocs serve
